2fda8008793f0df07f4a8b024bc4b67c4bedfb08
Butterscotch! <bscotchvanilla@gmail.com>
2024-02-20 03:42:02 -0500

    Automatically detect a Slime tracker and flash it

--- SlimeVrOta/Program.cs
+++ SlimeVrOta/Program.cs
@@ -1,14 +1,52 @@
+using System.Buffers.Binary;
 using System.Net;
+using System.Net.Sockets;
 using SlimeVrOta;
 
 var file = new FileInfo("C:/Users/Butterscotch/Downloads/ESPOTA/firmware-part-0.bin");
 
-EspOta
+var port = 6969;
+var endPoint = new IPEndPoint(IPAddress.Any, port);
+using var slimeClient = new UdpClient(port);
+
+Console.WriteLine("Waiting to receive tracker handshake...");
+
+var data = slimeClient.Receive(ref endPoint);
+var packetType = BinaryPrimitives.ReadUInt32BigEndian(data);
+
+// Handshake packet
+if (packetType != 3)
+{
+    throw new Exception($"Received a non-handshake packet on {port} from {endPoint}.");
+}
+Console.WriteLine($"Received a handshake packet on {port} from {endPoint}.");
+Console.WriteLine("Press enter to flash the tracker...");
+Console.ReadLine();
+
+var flashResult = EspOta
     .Serve(
-        new IPEndPoint(new IPAddress([192, 168, 4, 112]), 8266),
+        new IPEndPoint(endPoint.Address, 8266),
         file.Name,
         File.ReadAllBytes(file.FullName),
         "SlimeVR-OTA",
         EspOta.OtaCommands.FLASH
     )
-    .Wait();
+    .Result;
+
+if (!flashResult)
+{
+    throw new Exception($"Failed to flash tracker {endPoint}.");
+}
+
+Console.WriteLine("Waiting to receive post-flash handshake...");
+
+data = slimeClient.Receive(ref endPoint);
+packetType = BinaryPrimitives.ReadUInt32BigEndian(data);
+
+// Handshake packet
+if (packetType != 3)
+{
+    throw new Exception($"Received a non-handshake packet on {port} from {endPoint}.");
+}
+Console.WriteLine($"Received a handshake packet on {port} from {endPoint}.");
+Console.WriteLine($"Tracker {endPoint} has been flashed successfully");
